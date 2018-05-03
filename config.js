866146249b59581ea1048cb85d8825e99b64686f
yuvaroshit <38933993+yuvaroshit@users.noreply.github.com>
2018-05-03 17:00:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+var config={
+    database1: {
+        username:user1,
+        password:pwd1,
+        name:mongod
+    },
+    database2: {
+        username:user2,
+        password:pwd2,
+        name:mongodb
+    }
+};
+module.exports=config;
---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var cors=require('cors');
+var path=require('path');
+var router=express.Router();
+
+var env = process.env.NODE_ENV;
+var config = require('./config')[env];
+
+var app=express();
+
+const route=require('./routes/route');
+
+mongoose.connect('mongodb://localhost:27017/login');
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to database mongodb@27017');
+});
+
+mongoose.connection.on('error',(err)=>{
+    if(err)
+    {
+        console.log('Error in database connection:'+err);
+    }
+});
+
+const port=3000;
+
+app.use(cors());
+
+app.use(bodyparser.json());
+
+
+
+app.use('/api',route);
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/page.html'));
+});
+
+
+app.listen(port,()=>{
+    console.log('Server started at port:'+port);
+});
+
